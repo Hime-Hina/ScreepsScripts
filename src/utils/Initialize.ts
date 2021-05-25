1f9cc5344c58d8130b109525c596d0e0981590f3
HIMEHINA <1545340072@qq.com>
2021-05-25 09:13:13 +0800

    Add two functions

--- src/utils/Initialize.ts
+++ src/utils/Initialize.ts
@@ -1,68 +1,121 @@
-/* eslint-disable prettier/prettier */
-import { roleNames } from "Constants";
+import { creepsAmtAcquiredForEachFilling, creepsAmtAcquiredForEachRepairing, priorityStructureNeedToBeFilled, priorityStructureNeedToBeRepaired } from "Constants";
 
 const dirX = [-1, 1, 0, 0, -1, 1, -1, 1];
 const dirY = [0, 0, -1, 1, -1, 1, 1, -1];
 
-function ReturnAdjacentMiningPos(source: Source): MiningPos[] {
+const FindRCLContainer = (room: Room): StructureContainer | undefined | null => room.controller?.pos.findClosestByRange(FIND_STRUCTURES, {
+  filter: struct => struct.structureType === STRUCTURE_CONTAINER
+}) as (StructureContainer | undefined | null);
+
+const ReturnTowersId = (room: Room): Id<StructureTower>[] => {
+  const towers = room.find(FIND_STRUCTURES, { filter: struct => struct.structureType === STRUCTURE_TOWER }) as StructureTower[];
+  return towers.map(v => v.id);
+}
+
+export const FindTargetsToRepair = (room: Room): ITask => {
+  const structsToRepair = room.find(FIND_STRUCTURES, {
+    filter: struct => (struct.structureType in priorityStructureNeedToBeRepaired) && (struct.hits < struct.hitsMax)
+  });
+  const ret: ITask = {};
+  _.map(
+    (structsToRepair).sort(
+      (a, b) => {
+        const con0 = priorityStructureNeedToBeRepaired[a.structureType] - priorityStructureNeedToBeRepaired[b.structureType];
+        if (con0 === 0) {
+          return a.hits * b.hitsMax - a.hitsMax * b.hits;
+        } else return con0;
+      }
+    ),
+    struct => { return { id: struct.id, amtAcquired: creepsAmtAcquiredForEachRepairing[struct.structureType], finished: false } }
+  ).forEach(elem => ret[elem.id] = { amtAcquired: elem.amtAcquired, finished: elem.finished });
+  return ret;
+}
+
+export const FindTargetsToFill = (room: Room): ITask => {
+  const rclContainer = FindRCLContainer(room);
+  const structsToFill = room.find(FIND_STRUCTURES, {
+    filter: (struct: StructureNeedFill) => {
+      if (struct.structureType in priorityStructureNeedToBeFilled) {
+        if (struct.structureType === STRUCTURE_CONTAINER && struct.id !== rclContainer?.id) return false;
+        const newStore = struct.store as StoreDefinition;
+        return newStore.getUsedCapacity(RESOURCE_ENERGY) < newStore.getCapacity(RESOURCE_ENERGY);
+      } else return false;
+    }
+  }) as StructureNeedFill[];
+  const ret: ITask = {};
+
+  if (rclContainer) structsToFill.push(rclContainer);
+  _.map(
+    (structsToFill).sort(
+      (a: StructureNeedFill, b: StructureNeedFill) =>
+        (priorityStructureNeedToBeFilled[a.structureType] - priorityStructureNeedToBeFilled[b.structureType])
+    ),
+    struct => { return { id: struct.id, amtAcquired: creepsAmtAcquiredForEachFilling[struct.structureType], finished: false } }
+  ).forEach(elem => ret[elem.id] = { amtAcquired: elem.amtAcquired, finished: elem.finished });
+  return ret;
+}
+
+const ReturnAdjacentMiningPos = (source: Source): MiningPos[] => {
   const ret: MiningPos[] = [];
   let tmpPos: MiningPos;
 
   for (let i = 0; i < 8; ++i) {
     tmpPos = { x: source.pos.x + dirX[i], y: source.pos.y + dirY[i], isActive: true };
     if (
-      0 <= tmpPos.x && tmpPos.x < 50
-      && 0 <= tmpPos.y && tmpPos.y < 50
-      && source.room.getTerrain().get(source.pos.x, source.pos.y) === 0 // Plain terrain
-    ) {
-      ret.push(tmpPos);
-    }
+      (0 <= tmpPos.x && tmpPos.x < 50)
+      && (0 <= tmpPos.y && tmpPos.y < 50)
+      && (source.room.getTerrain().get(tmpPos.x, tmpPos.y) === 0) // Plain terrain
+    ) { ret.push(tmpPos); }
   }
   return ret;
 }
 
-function ReturnSourcesInfo(source: Source): SourceInfo {
-  return {
-    id: source.id,
-    miningPos: ReturnAdjacentMiningPos(source)
-  };
+const ReturnSourcesInfo = (room: Room): ISourcesInfo => {
+  const retVal: ISourcesInfo = {};
+  const roomSources = room.find(FIND_SOURCES);
+  for (const source of roomSources) {
+    retVal[source.id] = { activePosAmt: ReturnAdjacentMiningPos(source).length };
+  }
+  return retVal;
+}
+
+const FindSentryPos = (room: Room): { [id: string]: boolean } => {
+  const ramparts: { [id: string]: boolean } = {};
+  room.find(FIND_STRUCTURES, {
+    filter: (struct) => struct.structureType === STRUCTURE_RAMPART
+  }).forEach((rampart, idx, arr) => {
+    let isValid = true;
+    for (const ramId in ramparts) {
+      const storedRam = Game.getObjectById(ramId as Id<StructureRampart>);
+      if (storedRam && rampart.pos.inRangeTo(storedRam, 3)) {
+        isValid = false;
+        break;
+      }
+    }
+    if (isValid) ramparts[rampart.id] = true;
+  });
+  return ramparts;
 }
 
 export function Initialize(): void {
-  global.roomsInfo = {};
   // Store the information of each room's sources.
   for (const roomName in Game.rooms) {
-    global.roomsInfo[roomName] = {
-      creepsInfo: {
-        harvesters: [],
-        upgraders: [],
-        builder: [],
-        hostileCreeps: Game.rooms[roomName].find(FIND_HOSTILE_CREEPS)
-      },
-      sources: Game.rooms[roomName].find(FIND_SOURCES),
-      spawns: Game.rooms[roomName].find(FIND_MY_SPAWNS),
-      flags: Game.rooms[roomName].find(FIND_FLAGS)
+    Memory.rooms[roomName] = {
+      sources: {},
+      spawns: [],
+      flags: [],
+      sentries: FindSentryPos(Game.rooms[roomName]),
+      towers: ReturnTowersId(Game.rooms[roomName]),
+      rclContainerId: null,
+      targetsToRepair: FindTargetsToRepair(Game.rooms[roomName]),
+      targetsToFill: FindTargetsToFill(Game.rooms[roomName])
     };
-    for (const creep of Game.rooms[roomName].find(FIND_MY_CREEPS)) {
-      if (typeof creep.memory.role === "undefined") {
-        console.log(`${creep.name}<${creep.id}>, whose role was not assigned!`);
-      } else {
-        for (const roleName of roleNames) {
-          if (creep.memory.role === roleName) {
-            global.roomsInfo[roomName].creepsInfo[roleName]!.push(creep);
-          }
-        }
-      }
-    }
-
-    if (typeof Memory.rooms[roomName].sources === "undefined") {
-      Memory.rooms[roomName].sources = _.map(global.roomsInfo[roomName].sources, ReturnSourcesInfo);
-    }
-
-    // Memory.rooms[roomName].myCreeps = {};
-    // for (const creep of global.roomsInfo[roomName].myCreeps) {
-    //   Memory.rooms[roomName].myCreeps[creep.id] = { name: creep.name, role: creep.memory.role };
-    // }
-    // Memory.rooms[roomName].spawns = global.roomsInfo[roomName].spawns;
+    Memory.rooms[roomName].rclContainerId = FindRCLContainer(Game.rooms[roomName])?.id as (Id<StructureContainer> | null);
+    Memory.rooms[roomName].sources = ReturnSourcesInfo(Game.rooms[roomName]);
+    _.forIn(Game.creeps, (v) => {
+      if (v.memory.role) ++global.roleCounters[v.memory.role as Roles];
+    });
+    global.InitRolesMem(Game.rooms[roomName]);
+    console.log(`Room ${roomName} was initialized!`);
   }
 }
